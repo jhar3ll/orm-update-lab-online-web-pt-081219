f932f05ff14d01cd67109a9c3cd3ea8e949e96fb
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-11 02:48:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,5 +35,6 @@ attr_accessor :id, :name, :grade
     DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid FROM students")[0][0]
   end 
+end 
 
 end
